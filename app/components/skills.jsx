ed2bf6e599a03ecb3555c50562ddcf9621c3a32a
Fatih Yüksel <123635939+fatihyuksel3109@users.noreply.github.com>
2024-05-25 22:51:28 +0300

    update transition skills.jsx

--- app/components/skills.jsx
+++ app/components/skills.jsx
@@ -5,12 +5,12 @@ import React, { useState } from "react";
 const Skills = ({ data }) => {
   const [activeTab, setActiveTab] = useState("Hard");
 
-  const setBg = (active) => (activeTab === active ? "bg-blue-light text-black" : "bg-white text-blue");
+  const setBg = (active) => (activeTab === active ? "bg-blue-300 text-black" : "bg-white text-blue-600");
   const setTabsAlignment = (tab) =>
     tab === "Hard" ? "text-left" : "text-right";
 
   const tabs = (
-    <div className="flex justify-around ">
+    <div className="flex justify-around">
       {["Hard", "Soft"].map((el) => (
         <button
           key={el}
@@ -18,7 +18,7 @@ const Skills = ({ data }) => {
           className={`btn ${setBg(el)} ${setTabsAlignment(el)}`}
           onClick={() => setActiveTab(el)}
         >
-        <h3 className="">{el} Skills</h3>  
+          <h3>{el} Skills</h3>
         </button>
       ))}
     </div>
@@ -26,8 +26,8 @@ const Skills = ({ data }) => {
 
   const content = (
     <ul
-      className={`list-none py-4 gap-2 ${
-        activeTab === "Soft" ? "justify-start" : "justify-end"
+      className={`list-none py-4 gap-2 transition-opacity duration-500 ${
+        activeTab === "Soft" ? "opacity-0" : "opacity-100"
       }`}
     >
       {data[activeTab].map(({ icon, text }) => (
@@ -47,4 +47,3 @@ const Skills = ({ data }) => {
 };
 
 export default Skills;
-
